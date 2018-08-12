1d4ca9f9e84f0bb0d1063a4ec45080b231961ff6
AshenNeo <AshenNeo@gmail.com>
2018-08-12 22:56:34 +0900

    コミット漏れ対応

---
+++ TwitterApiTester/Twitter/GetAccessTokenResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace TwitterApiTester.Twitter
+{
+    public class GetAccessTokenResponse
+    {
+        public string oauth_token;
+        public string oauth_token_secret;
+        public string user_id;
+        public string screen_name;
+    }
+}
---
+++ TwitterApiTester/Twitter/SortedDictionaryHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace TwitterApiTester.Twitter
+{
+    public static class SortedDictionaryHelper
+    {
+        public static void AddUrlEncodedItem(this SortedDictionary<string, string> d, string key, string value)
+        {
+            d.Add(key, HttpUtility.UrlEncode(value));
+        }
+    }
+}
