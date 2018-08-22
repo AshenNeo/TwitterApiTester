7abd5b30f5df1124804e16854d1484e3619ccbfc
k.noji <k.noji@gloops.com>
2018-08-22 12:03:17 +0900

    ユーザーアクセストークンの保存を実装

--- TwitterApiTester/Pages/Index.cshtml.cs
+++ TwitterApiTester/Pages/Index.cshtml.cs
@@ -1,8 +1,12 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Options;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Session;
 using Tweetinvi;
 using Tweetinvi.Models;
 using Tweetinvi.Parameters;
@@ -25,6 +29,11 @@ namespace TwitterApiTester.Pages
         private const string QUERY_PARAM_AUTHORIZATION_ID = "authorization_id";
         private const long USER_ID = 133684052;   // サントリー公式のUserID
 
+        private const string SESSION_USER_ACCESS_TOKEN = "TwitterUserAccessToken";
+        private const string SESSION_USER_ACCESS_TOKEN_SECRET = "TwitterUserAccessTokenSecret";
+//        private const string SESSION_RETWEET_
+
+
         private readonly TwitterApiToken _twitterApiToken;
 
         public bool IsTwitterSignin { get; set; }
@@ -46,6 +55,27 @@ namespace TwitterApiTester.Pages
 
         public async Task OnGetAsync()
         {
+            // ユーザーアクセストークンが保存されている場合はそのまま使う
+            // 認証切れ等でトークンが無効な場合、Tweet実行結果が NULL となる。
+            var userAccessToken = HttpContext.Session.GetString(SESSION_USER_ACCESS_TOKEN);
+            var userAccessTokenSecret = HttpContext.Session.GetString(SESSION_USER_ACCESS_TOKEN_SECRET);
+            if (!string.IsNullOrEmpty(userAccessToken) && !string.IsNullOrEmpty(userAccessTokenSecret))
+            {
+                var userCreds = Auth.SetUserCredentials(_twitterApiToken.ConsumerApiKey,
+                    _twitterApiToken.ConsumerApiSecretKey, userAccessToken, userAccessTokenSecret);
+
+                Auth.ExecuteOperationWithCredentials(userCreds, () =>
+                {
+                    var tweet = Tweet.PublishTweet($"Authorized:{DateTime.Now}");
+                    if (tweet != null)
+                    {
+                        IsTwitterSignin = true;
+                        return;
+                    }
+                });
+            }
+
+            // 有効なアクセストークンが保存されていない場合は認証から実行する。
             if (Request.Query.ContainsKey(QUERY_PARAM_OAUTH_TOKEN) && Request.Query.ContainsKey(QUERY_PARAM_OAUTH_VERIFIER) && Request.Query.ContainsKey(QUERY_PARAM_AUTHORIZATION_ID))
             {
                 IsTwitterSignin = true;
@@ -64,6 +94,12 @@ namespace TwitterApiTester.Pages
             {
                 // 指定アカウントの最新Tweetを取得してリツイートする。
                 var userCreds = AuthFlow.CreateCredentialsFromVerifierCode(_requestTokenVerifier, _authorizationId);
+
+                // ユーザーのアクセストークンとシークレットを保存
+                HttpContext.Session.SetString(SESSION_USER_ACCESS_TOKEN, userCreds.AccessToken);
+                HttpContext.Session.SetString(SESSION_USER_ACCESS_TOKEN_SECRET, userCreds.AccessTokenSecret);
+
+
                 Auth.ExecuteOperationWithCredentials(userCreds, () =>
                 {
                     var timeline = Timeline.GetUserTimeline(USER_ID, new UserTimelineParameters()
